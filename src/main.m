8dd3ea7b891b3186bb9a182a0f2223ca4a78574d
Silke <silke.buchberger@hotmail.com>
2020-11-19 11:39:08 +0100

    cleaned up main
    still playing around with imregister

--- src/main.m
+++ src/main.m
@@ -6,113 +6,127 @@ classdef main
             addpath('../assets');
 %% SEGMENTIER-BILD
             
-%             binaryImage = BinaryImage.imageToBinary(image);
-%             h_im = imshow(image);
-            
+            % CREATE BINARY IMAGE
             mask = BinaryImage.imageToBinary(image);
-            
-            %newImage = createMask(mask, h_im);
-            %newImage(:,:,2) = newImage;
-            %newImage(:,:,3) = newImage(:,:,1);
-            
-            %ROI = image;
-            %ROI(newImage == 0) = 0;
             
             subplot(2,2,1);
             imshow(mask);
             
+            
+            % CREATE LABELED IMAGE
             [labeledImage, numOfLabels] = bwlabel(mask);
             subplot(2,2,2);
             imshow(label2rgb(labeledImage));
             
-            [boundaries, numberOfBoundaries]=Blobs.findBoundaries(mask);
             
-            subplot(2, 2, 3);
-            imshow(image);
-            axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
-            hold on;
-            for k = 1 : numberOfBoundaries
-                thisBoundary = boundaries{k};
-                plot(thisBoundary(:,2), thisBoundary(:,1), 'y', 'LineWidth', 1);
-            end
-            hold off;
+            % CALCULATE BOUNDARIES OF BINARY IMAGE
+%             [boundaries, numberOfBoundaries]=Blobs.findBoundaries(mask);
             
+%             subplot(2, 2, 3);
+%             imshow(image);
+%             axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+%             hold on;
+%             for k = 1 : numberOfBoundaries
+%                 thisBoundary = boundaries{k};
+%                 plot(thisBoundary(:,2), thisBoundary(:,1), 'y', 'LineWidth', 1);
+%             end
+%             hold off;
+            
+
+            % FIND RELEVANT BLOBS
             blobMeasurements = regionprops(labeledImage, 'all');
             numberOfBlobs = size(blobMeasurements, 1);
             
             allBlobOrientations = [blobMeasurements.Orientation];
-
-            allowableOrientIndexes = (allBlobOrientations > 65);
-            keeperIndexes = find(allowableOrientIndexes);
-            keeperBlobsImage = ismember(labeledImage, keeperIndexes);
             
             allowableIndexes = (allBlobOrientations > 65);
             keeperIndexes = find(allowableIndexes);
-            keeperMask = ismember(labeledImage, keeperIndexes);
-            keeperMask = bwmorph(keeperMask, 'bridge');
+            keeperMaskOrientation = ismember(labeledImage, keeperIndexes);
+            keeperLabel = bwlabel(keeperMaskOrientation);
+
+
+            % KEEP ONLY BIGGEST RELEVANT BLOB
+            allowableBlobsAreas = regionprops(keeperLabel, 'Area');
+            allowableBlobsAreas = [allowableBlobsAreas.Area];
+            biggestBlobArea = max(allowableBlobsAreas);
+            keeperAreas = find(allowableBlobsAreas == biggestBlobArea);
+            keeperMask = ismember(keeperLabel, keeperAreas);
             
-            subplot(2,2,4);
+            subplot(2,2,3);
             imshow(keeperMask);
             
+            
+            % USE MASK ON ORIGINAL IMAGE
             keeperBlobsImage = repmat(keeperMask,1,1,3);
             
-            segmentedImage = image; % Simply a copy at first.
-            segmentedImage(~keeperBlobsImage) = 0;  % Set all non-keeper pixels to zero.
+            segImageRGB = image; % copy of original image
+            segImageRGB(~keeperBlobsImage) = 0;  % set all non-keeper pixels to zero
 
+            
+            % FIND BOUNDING BOX & CROP IMAGE
             bBox = regionprops(keeperMask, 'BoundingBox').BoundingBox;
             disp(bBox)
             %points = bbox2points(bBox);
-            segmentedImage = imcrop(segmentedImage, bBox);
+            segImageRGB = imcrop(segImageRGB, bBox);
+            segImageBW = imcrop(keeperMask, bBox);
             %imshow(segmentedImage)
             
             subplot(2,2,4);
-            imshow(keeperMask);
+            imshow(segImageBW);
             
-            imwrite(segmentedImage, "../assets/segment.png");
+            
+            % SAVE CROPPED IMAGES
+            imwrite(segImageRGB, "../assets/segmentOriginal.png");
+            imwrite(segImageBW, "../assets/segmentBW.png");
 
 
 %% TEXTBILD - uncomment to compute skeleton for text image
+            
+%             % CREATE BINARY IMAGE
 %             binaryText = BinaryImage.imageToBinary(image);
 %             subplot(2,2,1);
 %             imshow(binaryText);
 %             
-%             skel = bwskel(binaryText, 'MinBranchLength', 50);
+%             % CREATE SKELETON
+%             skel = bwskel(binaryText, 'MinBranchLength', 50); % remove sidebranches
 %             subplot(2,2,2);
 %             imshow(skel);
-%             
-%             branchLengths = regionprops(skel, 'Area');
-%             branchLengths = [branchLengths.Area];
 %  
 %             subplot(2,2,4);
 %             imshow(labeloverlay(image,skel,'Transparency',0, 'Colormap', 'hot'));
 %             
 %             
-% % IMAGE REGISTRATION - versuch
+% %% IMAGE REGISTRATION - uncomment to register images
 % 
+%             % FIXED & MOVING HAVE TO BE RGB OR GRAYSCALE IMAGES
 %             fixedOriginal = skel;
 %             fixed = uint8(255 * skel);
-%             movingOriginal = imread("../assets/segment.png");
-%             moving = rgb2gray(movingOriginal);
-% %             moving(moving ~= 0) = 1;
-% %             moving = uint8(255 * moving);
+%             movingOriginal = imread("../assets/segmentOriginal.png");
+%             movingBW = imread("../assets/segmentBW.png");
+%             moving = uint8(255 * movingBW);
+%             %moving = rgb2gray(movingOriginal);
 %             
-% %             figure;
-% %             imshowpair(fixed,moving,'montage');
-%             
+%             % CREATE REGISTRATION CONFIGURATION DATA
 %             [optimizer, metric] = imregconfig('multimodal');
 %             
+%             % OPTIONAL ADJUSTMENTS
 %             %optimizer = registration.optimizer.OnePlusOneEvolutionary;
 %             %optimizer = registration.optimizer.RegularStepGradientDescent;
-%             %optimizer.InitialRadius = optimizer.InitialRadius * 0.8;
+%             optimizer.InitialRadius = optimizer.InitialRadius * 0.4;
 %             %metric = registration.metric.MattesMutualInformation;
 %             %metric = registration.metric.MeanSquares;
 %             
-%             tform = imregtform(moving, fixed, 'translation', optimizer, metric);
+%             
+%             % REGISTER IMAGES
 %             % 'rigid', 'similarity', 'translation' or 'affine'
-%             registered = imregister(moving, fixed, 'translation', optimizer, metric);
+%             registered = imregister(moving, fixed, 'rigid', optimizer, metric);
 %             figure;
 %             imshowpair(registered, fixed);
 %             
+%             % CALCULATE HOW IT WAS TRANSFORMED
+%             tform = imregtform(moving, fixed, 'rigid', optimizer, metric);
+%             
+%             % USE TRANSFORMATION ON ORIGINAL IMAGES
 %             registeredOriginal = imwarp(movingOriginal,tform,'OutputView',imref2d(size(fixed)));
 %             fusedOriginals = imfuse(registeredOriginal, fixedOriginal, 'blend');
 %             figure;
