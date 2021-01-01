4cae744d5d927101192ba34f614dfe45fbbd00ee
bedasue <72863121+bedasue@users.noreply.github.com>
2021-01-01 21:40:18 +0100

    main update

--- src/main.m
+++ src/main.m
@@ -1,67 +1,70 @@
 classdef main
     methods(Static)
-        function image = mainFunc(image) % need : mainFunc(imSegments, imText)
+        %% SEGMENTIER-BILD
+        function image = mainFuncObj(image)
             addpath('./helpers');
             addpath('../assets');
-%% SEGMENTIER-BILD
 
-%             %PRE-PROCESSING
-%             imageAdjusted = imadjust(image, [0.3 0.7], []);
-%             imageWithGauss = Filter.gaussFilter(imageAdjusted, 7);
-%             
-%             %CREATE BINARY IMAGE
-%             mask = Filter.imageToBinary(imageWithGauss, 0.85);
-%             
-%             %subplot(2,2,1);
-%             imshow(mask);
-%             
-%             %CREATE LABELED IMAGE -> need REGION GROWING instead
-%             [labeledImage, numOfLabels] = bwlabel(mask);
-%             
-%             %deviation array
-%             deviationsBlobs = zeros(1,numOfLabels); 
-%             
-%             %SAVE BLOBS IN CELL ARRAY
-%             blobs = cell(numOfLabels, 1); % contains all blobs
-%             for i = 1 : numOfLabels
-%                 blob = labeledImage == i;
-%                 
-%                 img = image;
-%                 mask = repmat(blob,1,1,3);
-%                 img(~mask) = 0;
-%                 
-%                 bBox = regionprops(blob, 'BoundingBox').BoundingBox;
-%                 img = imcrop(img, bBox);
-%                 blobs{i} = img;
-%                 
-%                 %skeleton & deviation from straight line
-%                 skelblob = bwskel(blob);
-% 
-%                 endpointsBlob = bwmorph(skelblob, 'endpoints');
-%                 [r1, c1] = find(endpointsBlob, 1, 'first'); % x, y of point A
-%                 [r2, c2] = find(endpointsBlob, 1, 'last');  % x, y of point B
-%                 %steigungBlob = (r2-r1)/(c2-c1);
-% 
-%                 numPixelsInBlob = int16(sum(sum(skelblob==1)));
-%                 [rowsLastHalfPixels, colsLastHalfPixels] = find(skelblob, numPixelsInBlob/2 , 'last');
-%                 rMiddle = rowsLastHalfPixels(1);
-%                 cMiddle = colsLastHalfPixels(1);
-% 
-%                 % berechnet vorerest nur Abstand der jeweiligen Pixel, die
-%                 % in der Mitte der Kurve bzw. Vergleichsgeraden liegen.
-%                 devX = (c1+c2)/2 - (cMiddle);
-%                 devY = (r1+r2)/2 - (rMiddle);
-%                 dev = norm([devX, devY]);
-%                 deviationsBlobs(i) = dev;
-%             end
-%          
-%             % provisorisch - kann sonst nicht auf die Werte zugreifen, wenn
-%             % der Teil auskommentiert ist
-%             writecell(blobs, 'blobscell.dat');
-%             writematrix(deviationsBlobs, 'deviationsBlobs.dat');
+            %PRE-PROCESSING
+            imageAdjusted = imadjust(image, [0.3 0.7], []);
+            imageWithGauss = Filter.gaussFilter(imageAdjusted, 7);
             
+            %CREATE BINARY IMAGE
+            mask = Filter.imageToBinary(imageWithGauss, 0.85);
+            
+            %subplot(2,2,1);
+            figure;
+            imshow(mask);
+            
+            %CREATE LABELED IMAGE -> need REGION GROWING instead
+            [labeledImage, numOfLabels] = bwlabel(mask);
+            
+            %deviation array
+            deviationsBlobs = zeros(1,numOfLabels); 
+            
+            %SAVE BLOBS IN CELL ARRAY
+            blobs = cell(numOfLabels, 1); % contains all blobs
+            for i = 1 : numOfLabels
+                blob = labeledImage == i;
+                
+                img = image;
+                mask = repmat(blob,1,1,3);
+                img(~mask) = 0;
+                
+                bBox = regionprops(blob, 'BoundingBox').BoundingBox;
+                img = imcrop(img, bBox);
+                blobs{i} = img;
+                
+                %skeleton & deviation from straight line
+                skelblob = bwskel(blob);
+
+                endpointsBlob = bwmorph(skelblob, 'endpoints');
+                [r1, c1] = find(endpointsBlob, 1, 'first'); % x, y of point A
+                [r2, c2] = find(endpointsBlob, 1, 'last');  % x, y of point B
+                %steigungBlob = (r2-r1)/(c2-c1);
+
+                numPixelsInBlob = int16(sum(sum(skelblob==1)));
+                [rowsLastHalfPixels, colsLastHalfPixels] = find(skelblob, numPixelsInBlob/2 , 'last');
+                rMiddle = rowsLastHalfPixels(1);
+                cMiddle = colsLastHalfPixels(1);
+
+                % berechnet vorerest nur Abstand der jeweiligen Pixel, die
+                % in der Mitte der Kurve bzw. Vergleichsgeraden liegen.
+                devX = (c1+c2)/2 - (cMiddle);
+                devY = (r1+r2)/2 - (rMiddle);
+                dev = norm([devX, devY]);
+                deviationsBlobs(i) = dev;
+            end
+         
+            % provisorisch - kann sonst nicht auf die Werte zugreifen, wenn
+            % der Teil auskommentiert ist
+            writecell(blobs, 'blobscell.dat');
+            writematrix(deviationsBlobs, 'deviationsBlobs.dat');
+            
+        end
 %% TEXTBILD - uncomment to compute skeleton for text image
             
+    function imageText = mainFuncText(image) % need : mainFunc(imSegments, imText)
             % CREATE BINARY IMAGE
             binaryText = BinaryImage.imageToBinary(image);
             %imshow(binaryText);
@@ -92,6 +95,7 @@ classdef main
                 [row1, col1] = find(endpoints, 1, 'first');
                 [row2, col2] = find(endpoints, 1, 'last');
                 img = Misc.modFloodFill(curve, [row1, col1], [row2, col2], 0);
+                figure;
                 imshow(img);
                 %steigung = (row2-row1)/(col2-col1);
                 
@@ -119,6 +123,7 @@ classdef main
                 end
             end
             
+            figure;
             imshowpair(closestBlob, image)
             
             %firstEndPoint = find(endPoints, 1, 'first');
@@ -136,6 +141,6 @@ classdef main
             %subplot(2,2,4);
             %imshow(labeloverlay(image, skel, 'Transparency', 0, 'Colormap', 'hot'));
             
-        end    
+        end   
      end
 end
