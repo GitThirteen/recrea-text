1585848fdeff8528975284d27b07f0a649f49a23
bedasue <72863121+bedasue@users.noreply.github.com>
2021-01-05 23:01:56 +0100

    update rotation

--- src/helpers/Transform.m
+++ src/helpers/Transform.m
@@ -42,10 +42,75 @@ classdef Transform
                end
 
               %angle between vectors of curve&blob (vectors of the verbindungsgeraden) 
-              angle = atan2d(vectorT(2), vectorT(1)) - atan2d(vectorB(2), vectorB(1));
+              angle = atan2(vectorT(2), vectorT(1)) - atan2(vectorB(2), vectorB(1));
               
               %rotate
-              imRotated = imrotate(blob, angle);
+              %imRotated = imrotate(blob, angle);
+               
+              %% this version works
+              [m,n]=size(blob,1:2);
+          
+               maxsize = max(m,n);
+                
+               mm = round(maxsize*sqrt(2));
+               nn = round(maxsize*sqrt(2));
+               imRotated = zeros(mm,nn,3);
+               for t=1:mm
+                  for s=1:nn
+                     i = uint16((t-round(mm/2))*cos(angle)+(s-round(nn/2))*sin(angle)+round(m/2));
+                     j = uint16(-(t-round(mm/2))*sin(angle)+(s-round(nn/2))*cos(angle)+round(n/2));
+                      if i>0 && j>0 && i<=m && j<=n           
+                         imRotated(t,s,:)=blob(i,j,:);
+                      end
+                   end
+               end
+  %% this version doesn't work, but wanted to do it with rotation matrix 
+              
+              % transpose rotation matrix
+%                matRotate = [cos(angle), sin(angle);
+%                              -sin(angle), cos(angle)];
+%                
+%                [nrows, ncols] = size(blob, 1:2);
+%                
+%                % create new image where rotated blob is put into 
+%                maxsize = max(nrows, ncols);
+%                maxsize = round(maxsize * sqrt(2));
+%                imRotated = zeros(maxsize, maxsize, 3);
+%                
+%                %center of imRotated
+%                middle = round(maxsize/2);
+%                
+%                % center of blob
+%                midRow = round(nrows/2);
+%                midCol = round(ncols/2); 
+%  
+%                % rotate around center
+%                [rowMat, colMat]  = meshgrid(1:maxsize, 1:maxsize);
+%        
+%                rowArr = reshape(rowMat, 1 , []);
+%                colArr = reshape(colMat, 1, []);
+%                
+% %                rowArr = repmat(rowArr,1,maxsize); %zeilenvec
+% %                colArr = repmat(colArr,1,maxsize); % zeilenvec
+%                
+%                coords = [rowArr-middle;colArr-middle]; % 2 x maxsize
+%                rotatedcoords = round(matRotate*coords);
+%                rotatedcoords(1,:) = rotatedcoords(1,:)+middle;
+%                rotatedcoords(2,:) = rotatedcoords(2,:)+middle;
+%                coords = [rowArr; colArr];
+%                  
+%               for l = 1:size(rotatedcoords,2)
+%                    [a,b] = transpose(rotatedcoords(:,l))
+%                    if a>0 && b>0 && a<=nrows && b<=ncols          
+%                      imRotated(coords(:,l),:)=blob(a,b,:);
+%                    end
+% %                         if  i > size(blob,1) | j > size(blob,2) | i<1 | j<1
+% %                             imRotated(r, c,:)=0;
+% %                         else
+% %                             imRotated(r,c,:) = blob(i,j,:);
+% %                         end
+% 
+%               end  
                
               return;
            end
@@ -56,6 +121,21 @@ classdef Transform
                
                imScaled = imresize(blob, factor);
                
+               
+            %%# Initializations:
+
+            % scale = [2 2];              %# The resolution scale factors: [rows columns]
+            % oldSize = size(inputImage);                   %# Get the size of your image
+            % newSize = max(floor(scale.*oldSize(1:2)),1);  %# Compute the new image size
+            % 
+            % %# Compute an upsampled set of indices:
+            % 
+            % rowIndex = min(round(((1:newSize(1))-0.5)./scale(1)+0.5),oldSize(1));
+            % colIndex = min(round(((1:newSize(2))-0.5)./scale(2)+0.5),oldSize(2));
+            % 
+            % %# Index old image to get new image:
+            % 
+            % outputImage = inputImage(rowIndex,colIndex,:);
            end
            
     end
