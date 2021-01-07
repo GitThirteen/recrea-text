4a30bc16707f98ddd53c0d0398ec60ed1624c82c
bitThirteen <mich1.eickmeyer@gmail.com>
2021-01-07 13:11:45 +0100

    rewrote comments in Algorithms class

--- src/helpers/Algorithms.m
+++ src/helpers/Algorithms.m
@@ -1,17 +1,58 @@
 classdef Algorithms
-    % algorithms to be used on a skeleton image
+    % ALGORITHMS
+    % A class containing useful algorithms necessary for the main file to
+    % run.
     %
-    % traceLine: for tracing the line along a skeleton image 
-    % curvature: find curvature properties of a skeleton
-    % findBranchpoints: locate branchpoints in a skeleton image
+    % Functions:
+    % > traceLine(skel, startP, endP, mode)
+    % A jumper function for trace(). Inputs different default values based
+    % on the <mode> parameter. Alternatively, trace() can be called
+    % directly to specify further details.
+    %
+    % > trace(skel, startP, endP, pts, ctr, index, threshold)
+    % A modified flood-fill algorithm used for tracing the line of a 
+    % specified skeleton. Starts at <startP> and checks every direction 
+    % (8-connectivity) if it contains a 1. If that's the case, it
+    % continues until either <endP> is reached or no other 1 is found. 
+    % Returns an N x 2 matrix containing the coordinates with the
+    % interval/steps of the <threshold> parameter.
+    %
+    % > curvature(skelblob, endpoints)
+    % find curvature properties of a skeleton
+    %
+    % > findBranchpoints(skel)
+    % Determines all branchpoints in a binary image (skeleton) by looping
+    % through the entire image provided through the <skel> parameter.
+    % 
+    % > isBranchpoint(skel, row, col)
+    % Helper function for findBranchpoints(). Runs through all pixels
+    % around a pixel specified by the <row> and <col> coordinate
+    % parameters and adds a 1 to an array if a pixel is 1. If the sum of
+    % 1's equals or exceeds 3, true is returned. If the sum equals 3 but
+    % there are adjacent 1's, false is returned to prevent a not entirely
+    % minimal skeleton from having too many branchpoints. For examples see
+    % below:
+    %
+    % 0 1 0                                         0 0 1
+    % 1 X 0 = true, because sum equals 3            0 X 0 = false, because sum < 3
+    % 0 1 0                                         0 1 0
+    %
+    % 0 1 0                                         0 1 1
+    % 1 X 1 = true, because sum > 3                 0 X 1 = false, because sum >= 3, but adjacent pixels
+    % 0 1 0                                         1 0 0
+    %
 
     methods(Static)
-%% traceLine
-
+           %% Line Tracing (Modified Flood Fill)
+           
+           % > Parameters:      
            % skel - a skeletonized blob/label
            % startP - the starting point of a passed-in line
            % endP - the end of a passed-in line
-           % pts - an array containing all seperation points
+           % mode - 'default' returns all points with an interval of 100 px, 'centerpt' returns the center of the line
+           % 
+           % > Returns:
+           % an N x 2 matrix containing all coordinates
            function result = traceLine(skel, startP, endP, mode)
                if (strcmp(mode, "default"))
                    result = Algorithms.trace(skel, startP, endP, zeros(100000,2), 0, 0, 100);
@@ -30,6 +71,17 @@ classdef Algorithms
                end
            end
            
+           % > Parameters:
+           % skel - a skeletonized blob/label
+           % startP - the starting point of a passed-in line
+           % endP - the end of a passed-in line
+           % pts - an N x 2 matrix for all coodinates
+           % ctr - counter variable, saves point at recursion intervals specified by <threshold>
+           % index - counter variable for array position
+           % threshold - interval for ctr, see above
+           %
+           % > Returns:
+           % an N x 2 matrix containing all coordinates
            function result = trace(skel, startP, endP, pts, ctr, index, threshold)
                sRow = startP(1);
                sCol = startP(2);
@@ -86,7 +138,7 @@ classdef Algorithms
                return;
            end
      
-%% curvature
+           %% Curvature
 
            % skelblob = a continuous skeleton
            % endpoints = its endpoints in the form [row1, col1, row2, col2]
@@ -119,8 +171,13 @@ classdef Algorithms
                 return;
            end
            
-%% findBranchpoints
+           %% Branchpoint Algorithm
 
+           % > Parameters:
+           % skel - a binary image (skeleton)
+           %
+           % > Returns:
+           % an N x 2 array containing all coodinates of branchpoints found
            function branchpoints = findBranchpoints(skel)
                branchpoints = zeros(1000, 2);
                index = 1;
@@ -141,6 +198,13 @@ classdef Algorithms
                branchpoints = branchpoints(1:index - 1, :);
            end
            
+           % > Parameters:
+           % skel - a binary image (skeleton)
+           % row - the row the checked pixel is at (X in examples above)
+           % col - the column the checked pixel is at (X in examples above)
+           %
+           % > Returns:
+           % True if branchpoint is found, false otherwise
            function result = isBranchpoint(skel, row, col)
                values = zeros(1, 9);
                index = 1;
